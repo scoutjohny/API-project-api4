0d4ad61d9620a268fd0809b829554834e1bf77fd
nikola <djordjevic.nikola.it@gmail.com>
2024-12-27 18:39:32 +0100

    Dodati endpointi getAllUsersByJsonPath i getAllUsersById

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Constants {
+
+    public static final String GET_ALL_USERS = "user";
+    public static final String GET_ALL_USERS_BY_ID = "user/{id}";
+}
--- src/test/java/tests/UsersTest.java
+++ src/test/java/tests/UsersTest.java
@@ -1,9 +1,10 @@
 package tests;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
+import utils.Constants;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,11 +25,47 @@ public class UsersTest {
                 .header("app-id", "675338db00236f7d1c311b6f")
                 .queryParams(map)
                 .log().all()
-                .when().get("user");
+                .when().get(Constants.GET_ALL_USERS);
 
-        SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(response.getStatusCode(),200, "Expected 200 but got: "+response.getStatusCode());
-        softAssert.assertAll();
+        Assert.assertEquals(response.getStatusCode(),200, "Expected 200 but got: "+response.getStatusCode());
+        String actualFirstName = response.jsonPath().get("data[0].firstName");
+        System.out.println(actualFirstName);
+        Assert.assertEquals(actualFirstName,"Edita");
+
+    }
+
+    @Test
+    public void getUsersUsingJsonPathTest() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("page",0);
+        map.put("limit",10);
+
+        JsonPath jsonPath  = given()
+                .baseUri("https://dummyapi.io/data")
+                .basePath("/v1/")
+                .header("app-id", "675338db00236f7d1c311b6f")
+                .queryParams(map)
+                .log().all()
+                .when().get(Constants.GET_ALL_USERS).jsonPath();
+
+
+        String actualFirstName = jsonPath.getString("data[0].firstName");
+        boolean result = actualFirstName.equals("Edita");
+        Assert.assertTrue(result,"Expected first name is not correct!");
+
+    }
+
+    @Test
+    public void getUsersByIdTest() {
+        String userId = "60d0fe4f5311236168a109ca";
+        Response response = given()
+                .baseUri("https://dummyapi.io/data")
+                .basePath("/v1/")
+                .header("app-id", "675338db00236f7d1c311b6f")
+                .pathParam("id",userId)
+                .log().all()
+                .when().get(Constants.GET_ALL_USERS_BY_ID);
+
         Assert.assertEquals(response.getStatusCode(),200, "Expected 200 but got: "+response.getStatusCode());
         String actualFirstName = response.jsonPath().get("data[0].firstName");
         System.out.println(actualFirstName);
